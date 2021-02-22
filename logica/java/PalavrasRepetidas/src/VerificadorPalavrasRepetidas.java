fc342a5688df605f33e53b67ffa656683d6e2ee3
caiocesarbrito <caio.25.brito@gmail.com>
2021-02-22 11:23:37 -0300

    Lógica Implementada

--- logica/java/PalavrasRepetidas/src/VerificadorPalavrasRepetidas.java
+++ logica/java/PalavrasRepetidas/src/VerificadorPalavrasRepetidas.java
@@ -1,9 +1,37 @@
+import java.util.ArrayList;
 
 public class VerificadorPalavrasRepetidas {
 
 	public String verificar(String palavra) {
 		// TODO Auto-generated method stub
-		return null;
-	}
+		
+		palavra = palavra.toLowerCase();
+		
+		ArrayList<String> letras_repetidas = new ArrayList<String>();
+			
+		for (int cont = 0; cont < palavra.length(); cont++ ) {
 
+			char letra_atual = palavra.charAt(cont);
+			
+			for (int cont2 = cont+1; cont2 < palavra.length(); cont2++) {
+				char letra_repeticao = palavra.charAt(cont2);
+				
+				if (letra_atual == letra_repeticao) {				
+					if (!letras_repetidas.contains(String.valueOf(letra_atual))) {
+						letras_repetidas.add(String.valueOf(letra_atual));
+					}
+				}
+			}
+		}
+			
+		String resultado;
+		
+		if (letras_repetidas.size() > 0) {
+			resultado = palavra + ": " + String.join(",", letras_repetidas);
+		} else {
+			resultado = palavra + ": none";
+		}
+		
+		return resultado;
+	}
 }
--- logica/java/Palindromo/src/Palindromo.java
+++ logica/java/Palindromo/src/Palindromo.java
@@ -2,7 +2,14 @@
 public class Palindromo {
 
 	public boolean ehPalindromo(String palavra) throws Exception {
-		throw new Exception("Você precisa implementar o código.");
+//		throw new Exception("Você precisa implementar o código.");
+		StringBuilder palavra_reversa = new StringBuilder(palavra).reverse();
+	    
+        if (palavra.equalsIgnoreCase(palavra_reversa.toString())) {
+        	return true;
+        } else {
+    		return false;
+        }
 	}
 
 }
